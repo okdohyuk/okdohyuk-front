379217d3b2b7e4b6cfb73d7de1db3c79bc0a840c
okdohyuk <okdohyuk@gmail.com>
2022-08-16 17:24:00 +0900

    [rtl] fix test file js to tsx

--- src/components/Card/__tests__/PercentCalculatorCard.test.tsx
+++ src/components/Card/__tests__/PercentCalculatorCard.test.tsx
@@ -4,6 +4,12 @@ import PercentCalculatorCard from '@components/Card/PercentCalculatorCard';
 import PercentStore from '@stores/PercentStore';
 import { Provider } from 'mobx-react';
 
+type TestElement = Document | Element | Window | Node;
+
+function hasInputValue(e: TestElement, inputValue: string) {
+  return screen.getByDisplayValue(inputValue) === e;
+}
+
 describe('<PercentCalculatorCard />', () => {
   it('matches snapshot', () => {
     const percentStore = new PercentStore();
@@ -36,8 +42,8 @@ describe('<PercentCalculatorCard />', () => {
     const result = screen.getByPlaceholderText('2,000');
     fireEvent.change(inputOne, { target: { value: '10000' } });
     fireEvent.change(inputTwo, { target: { value: '20' } });
-    expect(inputOne.value).toBe('10000');
-    expect(inputTwo.value).toBe('20');
-    expect(result.value).toBe('2,000');
+    expect(hasInputValue(inputOne, '10000')).toBe(true);
+    expect(hasInputValue(inputTwo, '20')).toBe(true);
+    expect(hasInputValue(result, '2,000')).toBe(true);
   });
 });
