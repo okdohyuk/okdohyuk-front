b42dfb1573f836311be4bc400c6284dcb8429acc
okdohyuk <okdohyuk@gmail.com>
2022-08-28 16:05:01 +0900

    [fix-list] fix locales nav margin bottom save area

--- src/components/Nav/LocalesNav.tsx
+++ src/components/Nav/LocalesNav.tsx
@@ -8,7 +8,7 @@ function LocalesNav() {
   return (
     <Link
       className={
-        'w-[57px] h-[57px] flex justify-center items-center cursor-pointer text-black dark:text-white fixed z-30 bottom-[77px] right-5 bg-zinc-100 dark:bg-zinc-800 lg:bottom-[340px] lg:right-5 lg:left-auto rounded-full shadow-md'
+        'w-[57px] h-[57px] mb-safe flex justify-center items-center cursor-pointer text-black dark:text-white fixed z-30 bottom-[77px] right-5 bg-zinc-100 dark:bg-zinc-800 lg:bottom-[340px] lg:right-5 lg:left-auto rounded-full shadow-md'
       }
       href={asPath}
       locale={locale === 'ko' ? 'en' : 'ko'}
