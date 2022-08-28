5947ad56e4468fe830c201f843c7d51d019142d5
okdohyuk <okdohyuk@gmail.com>
2022-08-28 15:44:46 +0900

    [i18next] feat locales nav toggle button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import * as gtag from '~/lib/gtag';
 import Nav from '@components/Nav';
 import { appWithTranslation } from 'next-i18next';
+import LocalesNav from '@components/Nav/LocalesNav';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -24,6 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider {...stores}>
       <Component {...pageProps} />
+      <LocalesNav />
       <Nav />
     </Provider>
   );
