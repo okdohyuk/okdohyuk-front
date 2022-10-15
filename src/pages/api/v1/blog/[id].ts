60f0f6073f014e3f78a980c374761ede3f9bc5b3
okdohyuk <okdohyuk@gmail.com>
2022-10-16 02:50:07 +0900

    [blog] feat get blog api

---
+++ src/pages/api/v1/blog/[id].ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+
+async function findBlog(id: string) {
+  try {
+    const blog = await client.blog.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!blog || !blog.isPublic) return null;
+    return blog;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  try {
+    if (!req.query.id) return res.status(400).json({ ok: false });
+    const blog = await findBlog(req.query.id + '');
+    if (blog === null) return res.status(404).json({ ok: false });
+
+    res.status(200).json({ ok: true, data: blog });
+  } catch (e) {
+    console.error('-> e', e);
+    res.status(500).json({ ok: false });
+  }
+}
+
+export default withHandler('GET', handler);
