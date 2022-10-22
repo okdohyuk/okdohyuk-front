3b0ef65093500667155c6a7ddce4f2e19c16b613
okdohyuk <okdohyuk@gmail.com>
2022-10-23 00:19:08 +0900

    [blog] fix blog card image src

--- src/components/Card/BlogCard.tsx
+++ src/components/Card/BlogCard.tsx
@@ -22,7 +22,7 @@ function BlogCard({ blog }: BlogCardProps) {
           {thumbnailImage ? (
             <div className={'w-[100px] h-[100px] md:w-[200px] md:h-[200px] relative'}>
               <Image
-                src={process.env.NEXT_PUBLIC_VERCEL_URL + thumbnailImage}
+                src={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}${thumbnailImage}`}
                 layout={'fill'}
                 objectFit={'cover'}
               />
