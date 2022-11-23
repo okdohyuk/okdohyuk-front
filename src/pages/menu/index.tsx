7fb4a5721674eb3cbfeb58a4885165ad3f224ec8
okdohyuk <okdohyuk@gmail.com>
2022-11-23 18:36:43 +0900

    [menu] feat menu page ogtag

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import CommonLayout from '@components/Complex/Layouts/CommonLayout';
+import { useTranslation } from 'next-i18next';
+import { GetStaticPropsContext } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 function MenuPage() {
-  return <CommonLayout>{''}</CommonLayout>;
+  const { t } = useTranslation('menu');
+  return (
+    <CommonLayout
+      title={t('openGraph.title')}
+      ogTitle={t('openGraph.ogTitle')}
+      description={t('openGraph.description')}
+    >
+      {''}
+    </CommonLayout>
+  );
 }
 
 export default MenuPage;
+
+export const getStaticProps = async ({ locale }: GetStaticPropsContext) => ({
+  props: {
+    ...(await serverSideTranslations(locale as string, ['common', 'menu'])),
+  },
+});
