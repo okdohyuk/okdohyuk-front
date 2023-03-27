b6912704ea5e2f5709219b09c28ba8a5b8ae08cf
Dohyuk You <okdohyuk@gmail.com>
2023-03-27 13:17:36 +0000

    [menu] refactor menus json

--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -3,82 +3,9 @@ import { useTranslation } from 'next-i18next';
 import { GetStaticPropsContext } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Opengraph from '@components/Basic/Opengraph';
-import Link from '~/components/Basic/Link';
-import {
-  MdCalculate,
-  MdViewList,
-  MdTagFaces,
-  MdOutlinePersonalVideo,
-  MdPayment,
-} from 'react-icons/md';
-import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
+import Link from '@components/Basic/Link';
 import { useRouter } from 'next/router';
-
-type MenuItem = {
-  title: string;
-  titlen: string;
-  icon: React.ReactNode;
-  link: string;
-};
-
-type Menus = {
-  [key: string]: MenuItem[];
-};
-
-const menus: Menus = {
-  service: [
-    {
-      title: '퍼센트계산기',
-      titlen: 'Percent Calculator',
-      icon: <MdCalculate />,
-      link: '/percent',
-    },
-    {
-      title: '할일',
-      titlen: 'Todo',
-      icon: <MdViewList />,
-      link: '/todo',
-    },
-  ],
-  out: [
-    {
-      title: '프로필',
-      titlen: 'Profile',
-      icon: <MdTagFaces />,
-      link: 'https://okdohyuk.notion.site/',
-    },
-    {
-      title: '블로그',
-      titlen: 'Blog',
-      icon: <MdOutlinePersonalVideo />,
-      link: 'https://blog.okdohyuk.dev/',
-    },
-    {
-      title: '깃허브',
-      titlen: 'Github',
-      icon: <AiFillGithub />,
-      link: 'https://github.com/okdohyuk',
-    },
-    {
-      title: '링크드인',
-      titlen: 'Linkedin',
-      icon: <AiFillLinkedin />,
-      link: 'https://www.linkedin.com/in/okdohyuk/',
-    },
-    {
-      title: '유튜브',
-      titlen: 'Youtube',
-      icon: <AiFillYoutube />,
-      link: 'https://www.youtube.com/@okdohyuk',
-    },
-    {
-      title: '후원하기',
-      titlen: 'Donate',
-      icon: <MdPayment />,
-      link: 'https://toss.me/guksu',
-    },
-  ],
-};
+import { MenuItem, Menus, menus } from '@assets/datas/menus';
 
 function MenuPage() {
   const { t } = useTranslation('menu');
