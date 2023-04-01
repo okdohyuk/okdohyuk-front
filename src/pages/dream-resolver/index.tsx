96a5a3f50dcb43192db01678aa1bc2c3f4fc2a5e
Dohyuk You <okdohyuk@gmail.com>
2023-04-01 09:46:18 +0000

    [dream-resolver] feat share

--- src/pages/dream-resolver/index.tsx
+++ src/pages/dream-resolver/index.tsx
@@ -5,8 +5,20 @@ import React, { useEffect } from 'react';
 import Opengraph from '@components/Basic/Opengraph';
 import Cookies from 'js-cookie';
 import { AiOutlineLoading } from 'react-icons/ai';
+import { MdLink } from 'react-icons/md';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import Link from '@components/Basic/Link';
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  LineShareButton,
+  LineIcon,
+} from 'react-share';
 
 function DreamResolver() {
   const { locale } = useRouter();
@@ -15,6 +27,22 @@ function DreamResolver() {
   const [dreamResult, setDreamResult] = React.useState<string>('');
   const [loading, setLoading] = React.useState<boolean>(true);
   const [isTodayResolved, setIsTodayResolved] = React.useState<boolean>(false);
+  const currentUrl = 'https://okdohyuk.dev/dream-resolver';
+
+  const handle = () => {
+    if (navigator.share) {
+      navigator.share({
+        title: t('openGraph.title'),
+        text: t('openGraph.description'),
+        url: currentUrl,
+      });
+    } else if (navigator.clipboard) {
+      navigator.clipboard.writeText(currentUrl);
+      alert(t('copyed'));
+    } else {
+      alert(t('failShare'));
+    }
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -42,6 +70,7 @@ function DreamResolver() {
       );
     } catch (error) {
       console.error(error);
+      alert(t('error'));
     } finally {
       setLoading(false);
     }
@@ -49,7 +78,7 @@ function DreamResolver() {
 
   useEffect(() => {
     const { dreamContent, dreamResult } = JSON.parse(Cookies.get('dream-resolver') || '{}');
-    if (dreamContent.length > 0 && dreamResult.length > 0) {
+    if (dreamContent && dreamContent.length > 0 && dreamResult && dreamResult.length > 0) {
       setDreamContent(dreamContent);
       setDreamResult(dreamResult);
       setIsTodayResolved(true);
@@ -65,7 +94,7 @@ function DreamResolver() {
         description={t('openGraph.description')}
         image={'/dream-resolver.jpg'}
       />
-      <main className="flex flex-col items-center w-full lg:w-96 bg-zinc-100 dark:bg-zinc-800 mx-auto lg:mt-10 h-auto lg:rounded-lg p-4">
+      <main className="flex flex-col items-center w-full h-screen lg:h-auto lg:mx-auto lg:w-96 bg-zinc-100 dark:bg-zinc-800 lg:my-auto lg:rounded-lg p-4">
         <h1 className="text-xl lg:text-2xl font-bold mb-6 text-black dark:text-white">
           {t('openGraph.title')}
         </h1>
@@ -93,6 +122,10 @@ function DreamResolver() {
                 {dreamResult}
               </span>
             </div>
+            <span className="my-4 text-black dark:text-white">{t('tryAgain')}</span>
+            <Link href={'https://toss.me/guksu'} hasTargetBlank>
+              <span className="text-black dark:text-white">{t('donate')}</span>
+            </Link>
           </>
         )}
         {!loading && !isTodayResolved && (
@@ -112,6 +145,26 @@ function DreamResolver() {
             </button>
           </form>
         )}
+        <div className="flex w-full justify-around mt-6">
+          <FacebookShareButton url={currentUrl}>
+            <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+          </FacebookShareButton>
+          <FacebookMessengerShareButton url={currentUrl} appId="622375919312906">
+            <FacebookMessengerIcon size={48} round={true} borderRadius={24}></FacebookMessengerIcon>
+          </FacebookMessengerShareButton>
+          <TwitterShareButton url={currentUrl}>
+            <TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
+          </TwitterShareButton>
+          <LineShareButton url={currentUrl}>
+            <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
+          </LineShareButton>
+          <button
+            onClick={handle}
+            className="flex items-center justify-center w-[48px] h-[48px] rounded-full bg-zinc-500"
+          >
+            <MdLink size={32} className="text-white" />
+          </button>
+        </div>
       </main>
     </>
   );
