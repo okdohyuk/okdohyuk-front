5d19387513f1f36c517fc1fec9ee7912b851f0c8
Dohyuk You <root@okdohyuk.dev>
2023-08-14 13:55:39 +0000

    feat input style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        point: {
+          1: '#BEADFA',
+          2: '#D0BFFF',
+          3: '#DFCCFB',
+        },
+      },
+    },
   },
   darkMode: 'media',
   plugins: [require('tailwindcss-safe-area'), require('@tailwindcss/line-clamp')],
