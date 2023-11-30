41e8f10cfbce4510c7d706b1dfc3ff2cdefcc365
Dohyuk You <root@okdohyuk.dev>
2023-11-30 10:02:47 +0000

    ssg/fix: blog param path

--- src/pages/blog/[urlSlug].tsx
+++ src/pages/blog/[urlSlug].tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GetStaticPaths, NextPageContext } from 'next';
+import { GetStaticPaths, GetStaticPropsContext } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Blog } from '@api/Blog';
 import markdownUtils from '@utils/markdownUtils';
@@ -131,14 +131,14 @@ function BlogDetailPage({ blog }: BlogPageProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const { data: blogs } = await blogApi.getBlog(0, 1000);
-  const paths = blogs.map((blog) => ({ params: { blogId: blog.id + '' } }));
+  const paths = blogs.map((blog) => ({ params: { urlSlug: blog.urlSlug } }));
 
   return { paths, fallback: 'blocking' };
 };
- 
-export async function getStaticProps({ query, locale }: NextPageContext) {
-  if (!(query && query.urlSlug)) return { notFound: true };
-  const { urlSlug } = query;
+
+export async function getStaticProps({ params, locale }: GetStaticPropsContext) {
+  if (!(params && params.urlSlug)) return { notFound: true };
+  const { urlSlug } = params;
   try {
     const { data: blog } = await blogApi.getBlogUrlSlug(urlSlug + '');
     const translations = await serverSideTranslations(locale as string, ['common', 'blog']);
