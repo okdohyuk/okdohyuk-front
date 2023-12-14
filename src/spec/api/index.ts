1643aefbddce7cce14368bc3aadd26bbfb0723f3
Dohyuk You <root@okdohyuk.dev>
2023-12-14 14:44:44 +0000

    auth/feat: update api spec

--- src/spec/api/index.ts
+++ src/spec/api/index.ts
@@ -1,3 +1,7 @@
+import { AuthApi } from './Auth';
 import { BlogApi } from './Blog';
+import { UserApi } from './User';
 
 export const blogApi = new BlogApi();
+export const userApi = new UserApi();
+export const authApi = new AuthApi();
