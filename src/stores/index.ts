aeb9567e1047c284baa62adb91db21679276109f
Dohyuk You <root@okdohyuk.dev>
2023-12-16 02:56:52 +0000

    auth/feat: user store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,6 +2,7 @@ import { enableStaticRendering } from 'mobx-react';
 import TodoStore from '@stores/TodoStore';
 import PercentStore from '@stores/PercentStore';
 import BlogStore from '@stores/BlogStore';
+import UserStore from '@stores/UserStore';
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 enableStaticRendering(typeof window === 'undefined');
@@ -10,4 +11,5 @@ export default {
   todoStore: new TodoStore(),
   percentStore: new PercentStore(),
   blogStore: new BlogStore(),
+  userStore: new UserStore(),
 };
--- src/stores/type.ts
+++ src/stores/type.ts
@@ -1,9 +1,11 @@
 import { TodoStoreState } from '@stores/TodoStore/type';
 import { PercentStoreState } from '@stores/PercentStore/type';
 import { BlogStoreState } from '@stores/BlogStore/type';
+import { UserStoreState } from '@stores/UserStore/type';
 
 export interface Store {
   todoStore: TodoStoreState;
   percentStore: PercentStoreState;
   blogStore: BlogStoreState;
+  userStore: UserStoreState;
 }
