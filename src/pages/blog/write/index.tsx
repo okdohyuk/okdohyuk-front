970d6bdc341c57d59ec103464f9092b991c9ce3d
Dohyuk You <root@okdohyuk.dev>
2023-12-21 14:48:35 +0000

    feat: blog write page image upload

--- src/pages/blog/write/index.tsx
+++ src/pages/blog/write/index.tsx
@@ -2,23 +2,27 @@ import React from 'react';
 import { GetServerSideProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Blog } from '@api/Blog';
-import { withTranslation } from 'next-i18next';
-import { blogApi, userApi } from '@api';
+import { useTranslation, withTranslation } from 'next-i18next';
+import { blogApi, storageApi, userApi } from '@api';
 import { UserRoleEnum } from '~/spec/api/User';
 import Jwt from '~/utils/jwtUtils';
 import BlogPost from '~/components/Complex/Blog';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/router';
+import Opengraph from '~/components/Basic/Opengraph';
+import { MdImage } from 'react-icons/md';
 
 type BlogPageProps = {
   blog: Blog | null;
 };
 
-function BlogEditPage({ blog }: BlogPageProps) {
+function BlogWritePage({ blog }: BlogPageProps) {
+  const { t } = useTranslation('blog/write');
   const { register, watch, handleSubmit } = useForm<Blog>({
     defaultValues: blog ? blog : { createdAt: new Date().toString(), contents: '' },
   });
+  const [imageUrl, setImageUrl] = React.useState<string>('');
   const { push } = useRouter();
 
   const onSubmit: SubmitHandler<Blog> = async (submitBlog) => {
@@ -29,49 +33,95 @@ function BlogEditPage({ blog }: BlogPageProps) {
       } else {
         await blogApi.putBlogUrlSlug('Bearer ' + accessToken, blog.urlSlug, submitBlog);
       }
-      push('/blog/' + submitBlog.title.replaceAll(' ', '-'));
+      if (submitBlog.isPublic) {
+        push('/blog/' + submitBlog.title.replaceAll(' ', '-'));
+      } else {
+        push('/admin/blog');
+      }
+    } catch (e) {
+      console.error('-> e', e);
+    }
+  };
+
+  const onClickHandler = (e: React.MouseEvent<HTMLLabelElement, MouseEvent>) => {
+    if (e.target !== e.currentTarget) e.currentTarget.click();
+  };
+
+  const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    const accessToken = Cookies.get('access_token');
+    try {
+      const { data } = await storageApi.postStorageImage('Bearer ' + accessToken, file);
+      setImageUrl(data);
     } catch (e) {
       console.error('-> e', e);
     }
   };
 
   return (
-    <div className="flex w-full h-screen overflow-hidden">
-      <div className="flex-1 flex flex-col relative pb-16">
-        <input
-          className="w-full input-text"
-          id="title"
-          placeholder="title"
-          {...register('title', {
-            required: true,
-          })}
-        />
-        <input className="" type="checkbox" id="isPublic" {...register('isPublic')} />
-        <input
-          className="w-full input-text"
-          id="thumbnailImage"
-          placeholder="thumbnailImage"
-          {...register('thumbnailImage')}
-        />
-        <textarea
-          className="w-full h-full resize-none input-text"
-          id="content"
-          {...register('contents', {
-            required: true,
-          })}
-        />
-        <div className="w-full lg:w-1/2 flex flex-row-reverse items-center fixed bottom-[57px] lg:bottom-0 left-0 h-16 bg-basic-5 px-4">
-          <button className="button h-8" onClick={handleSubmit(onSubmit)}>
-            Post
-          </button>
+    <>
+      <Opengraph title={'Blog Edit'} ogTitle={'Blog Edit'} description={'Blog Edit'} />
+      <div className="flex w-full h-screen overflow-hidden">
+        <div className="flex-1 flex flex-col relative pb-16">
+          <div className="flex flex-col p-4 gap-4">
+            <input
+              className="input-text"
+              id="title"
+              placeholder={t('form.title')}
+              {...register('title', {
+                required: true,
+              })}
+            />
+            <input
+              className="input-text"
+              id="thumbnailImage"
+              placeholder={t('form.thumbnailImage')}
+              {...register('thumbnailImage')}
+            />
+            <div className="flex gap-2">
+              {/* 이미지 업로드 */}
+              <input className="input-text flex-1" value={imageUrl} />
+              <div>
+                <label className="" htmlFor={'upload-image'} onClick={onClickHandler}>
+                  <button className="button h-[38px]">
+                    <MdImage size={24} />
+                  </button>
+                </label>
+                <input
+                  className="hidden"
+                  id={'upload-image'}
+                  type="file"
+                  accept="image/png, image/jpeg"
+                  multiple
+                  onChange={uploadImage}
+                />
+              </div>
+            </div>
+          </div>
+
+          <textarea
+            className="w-full h-full resize-none input-text outline-none rounded-none"
+            id="content"
+            {...register('contents', {
+              required: true,
+            })}
+          />
+          <div className="w-full lg:w-1/2 flex justify-between items-center fixed bottom-[57px] lg:bottom-0 left-0 h-16 bg-basic-5 px-4">
+            <input className="" type="checkbox" id="isPublic" {...register('isPublic')} />
+            <button className="button h-8" onClick={handleSubmit(onSubmit)}>
+              {t('post')}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex-1 hidden lg:block overflow-y-scroll">
+          <div className="relative lg:px-6 lg:py-6 box-border bg-zinc-100 dark:bg-zinc-900">
+            <BlogPost blog={watch()} />
+          </div>
         </div>
       </div>
-      <div className="flex-1 hidden lg:block overflow-y-scroll">
-        <div className="relative lg:px-6 lg:py-6 box-border bg-zinc-100 dark:bg-zinc-900">
-          <BlogPost blog={watch()} />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
@@ -91,7 +141,7 @@ export const getServerSideProps: GetServerSideProps = async ({
     const { data: user } = await userApi.getUserUserId('Bearer ' + accessToken, tokenPayload.id);
     if (user.role !== UserRoleEnum.Admin) return { notFound: true };
 
-    const translations = await serverSideTranslations(locale as string, ['common', 'blog']);
+    const translations = await serverSideTranslations(locale + '', ['common', 'blog/write']);
 
     if (!urlSlug)
       return {
@@ -101,7 +151,7 @@ export const getServerSideProps: GetServerSideProps = async ({
         },
       };
 
-    const { data: blog } = await blogApi.getBlogUrlSlug(urlSlug + '');
+    const { data: blog } = await blogApi.getBlogUrlSlug(urlSlug + '', 'Bearer ' + accessToken);
 
     return {
       props: {
@@ -115,4 +165,4 @@ export const getServerSideProps: GetServerSideProps = async ({
   }
 };
 
-export default withTranslation(['common', 'blog'])(BlogEditPage);
+export default withTranslation(['common', 'blog/write'])(BlogWritePage);
