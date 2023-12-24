3efd2bb3c635a8bc65da093ed45a527bd168c661
Dohyuk You <root@okdohyuk.dev>
2023-12-24 10:47:22 +0000

    Update Google Analytics tracking ID

--- src/libs/client/gtag.ts
+++ src/libs/client/gtag.ts
@@ -1,8 +1,6 @@
-export const GA_TRACKING_ID = 'G-Z9H87L43J1';
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  window.gtag('config', `${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`, {
     page_path: url,
   });
 };
