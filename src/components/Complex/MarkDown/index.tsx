30010c2049fcaf65594212304968dbab7bf3eba8
okdohyuk <root@okdohyuk.dev>
2023-12-28 19:32:03 +0900

    fix: blog link target blank

--- src/components/Complex/MarkDown/index.tsx
+++ src/components/Complex/MarkDown/index.tsx
@@ -4,6 +4,7 @@ import Aside from './Aside';
 import CodeWindow from './CodeWindow';
 import { MarkDownProps } from './type';
 import Table from './Table';
+import Link from '@components/Basic/Link';
 
 const MarkDown: MarkDownProps = ({ contents }) => {
   return (
@@ -21,6 +22,12 @@ const MarkDown: MarkDownProps = ({ contents }) => {
           },
           table: {
             component: Table,
+          },
+          a: {
+            component: Link,
+            props: {
+              hasTargetBlank: true,
+            },
           },
         },
       }}
