6e99cc39dd7f3c9f269ce3a5f9ece570657670a7
okdohyuk <root@okdohyuk.dev>
2023-12-28 19:28:26 +0900

    fix: table component fixed

--- src/components/Complex/MarkDown/Table.tsx
+++ src/components/Complex/MarkDown/Table.tsx
@@ -4,7 +4,7 @@ import { MarkdownComponent } from './type';
 const Table: MarkdownComponent = ({ children, ...props }) => {
   return (
     <div className={'flex overflow-x-scroll my-4'}>
-      <table {...props} className={'table table-auto m-0 break-keep'}>
+      <table {...props} className={'table table-fixed m-0 break-keep'}>
         {children}
       </table>
     </div>
