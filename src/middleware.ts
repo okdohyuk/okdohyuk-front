68aa24d0aef1435fb9be2b41647aa9c4b4d93a50
Dohyuk You <root@okdohyuk.dev>
2024-01-20 02:20:33 +0000

    blog-search/fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,18 @@ import type { NextRequest } from 'next/server';
 import { userApi } from '@api';
 import Jwt from '@utils/jwtUtils';
 import { UserRoleEnum } from '@api/User';
+import UserTokenUtil from './utils/userTokenUtil';
 
 export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('access_token');
 
   if (request.nextUrl.pathname.startsWith('/admin')) {
-
-    if (!accessToken) return NextResponse.rewrite(new URL('/404', request.url));
-    const jwt = Jwt;
-    const tokenPayload = jwt.getPayload(accessToken);
+    if (!accessToken) return NextResponse.redirect(new URL('/auth/login', request.url));
+    const tokenPayload = Jwt.getPayload(accessToken);
     if (tokenPayload && !tokenPayload.id) return NextResponse.rewrite(new URL('/404', request.url));
 
     userApi
-      .getUserUserId(accessToken, tokenPayload.id)
+      .getUserUserId(UserTokenUtil.getTokenBearer(accessToken), tokenPayload.id)
       .then(({ data: user }) => {
         if (user.role === UserRoleEnum.Admin) return NextResponse.next();
         throw new Error('Not admin');
