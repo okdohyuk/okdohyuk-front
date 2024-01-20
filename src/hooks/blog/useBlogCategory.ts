d3767eca18a3e21b411c1c2e7831b8aec4730389
Dohyuk You <root@okdohyuk.dev>
2024-01-20 02:20:00 +0000

    blog-search/feat: admin blog category page

---
+++ src/hooks/blog/useBlogCategory.ts
@@ -0,0 +1,38 @@
+import { blogApi } from '~/spec/api';
+import { PostBlogCategoryRequest } from '~/spec/api/Blog';
+import { UseCategoryProps } from './type';
+import { accessToken } from '~/utils/userTokenUtil';
+
+const useBlogCategory = () => {
+  const post = async ({ category, parent }: UseCategoryProps) => {
+    if (category.trim() === '') throw new Error();
+
+    const postdata: PostBlogCategoryRequest = {
+      name: category,
+      parentId: parent !== null ? parent.id : undefined,
+    };
+
+    await blogApi.postBlogCategory(accessToken, postdata);
+  };
+
+  const patch = async ({ parent }: UseCategoryProps) => {
+    if (!(parent && parent.id && parent.name)) return;
+
+    const postdata: PostBlogCategoryRequest = {
+      name: parent.name,
+    };
+
+    await blogApi.patchBlogCategoryId(accessToken, parent.id, postdata);
+  };
+
+  const remove = async ({ parent }: UseCategoryProps) => {
+    if (!(parent && parent.id && parent.name)) return;
+    if (!window.confirm('정말 삭제 하시겠습니까?')) return;
+
+    await blogApi.deleteBlogCategoryId(accessToken, parent.id);
+  };
+
+  return { post, patch, remove };
+};
+
+export default useBlogCategory;
