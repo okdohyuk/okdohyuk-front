9a2cfad6bbe37fcc8b44ca9d0c0df8c151302acd
Dohyuk You <root@okdohyuk.dev>
2024-01-23 10:07:07 +0000

    blog-search/feat: export FilterDropdown type

--- src/components/complex/FilterDropdown/type.ts
+++ src/components/complex/FilterDropdown/type.ts
@@ -1,16 +1,18 @@
+type FilterType = 'in' | 'notIn' | 'idle';
+
 type FilterDropdownItem = {
   name: string;
   value: string;
   child?: FilterDropdownItem[];
-  type: 'in' | 'notIn' | 'idle';
+  type: FilterType;
 };
 
 type FilterDropdownProps = {
   title: string;
   items: FilterDropdownItem[];
-  changeType: (value: string, type: 'in' | 'notIn' | 'idle') => void;
+  changeType: (value: string, type: FilterType) => void;
 };
 
 type FilterDropdownFC = React.FC<FilterDropdownProps>;
 
-export type { FilterDropdownFC, FilterDropdownItem };
+export type { FilterDropdownFC, FilterDropdownItem, FilterType };
