49ff73f1b781994e6af222545c86fad00eb28b7a
Dohyuk You <root@okdohyuk.dev>
2024-04-14 09:29:39 +0000

    blog-toc/fix: remove unused import

--- src/libs/client/heading.ts
+++ src/libs/client/heading.ts
@@ -1,32 +1,4 @@
-import StringUtils from '@utils/stringUtils';
-import { BlogToc } from '~/components/blog/BlogDetail/type';
-
-/**
- * Set unique id for each headings (only for h1, h2, h3)
- * @param html
- */
-export function setHeadingId(html: string) {
-  const div = document.createElement('div');
-  div.innerHTML = html;
-
-  const h1 = div.querySelectorAll('h1');
-  const h2 = div.querySelectorAll('h2');
-  const h3 = div.querySelectorAll('h3');
-
-  const idList: string[] = [];
-
-  const setId = (element: HTMLHeadingElement) => {
-    const id = StringUtils.toUrlSlug(element.innerText);
-    const exists = idList.filter((existingId) => existingId.indexOf(id) !== -1);
-    const uniqueId = `${id}${exists.length === 0 ? '' : `-${exists.length}`}`;
-    element.id = uniqueId;
-    idList.push(uniqueId);
-  };
-
-  [h1, h2, h3].forEach((elements) => elements.forEach(setId));
-
-  return div.innerHTML;
-}
+import { BlogToc } from '@components/blog/BlogDetail/type';
 
 /**
  * Parses heading for building TOC
