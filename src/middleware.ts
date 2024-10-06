b4d6ab58cd294abc4e11c9b63c05732efb6608c4
DOHYUK YOU <okdohyuk@gmail.com>
2024-10-06 22:56:48 +0900

    fix: middleware.ts remove admin Authorization logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import acceptLanguage from 'accept-language';
-import { userApi } from '@api';
-import Jwt from '@utils/jwtUtils';
-import { UserRoleEnum } from '@api/User';
-import UserTokenUtil from './utils/userTokenUtil';
 import { cookieName, fallbackLng, languages } from '~/app/i18n/settings';
 
 acceptLanguage.languages([...languages]);
 
 export function middleware(req: NextRequest) {
-  const accessToken = req.cookies.get('access_token');
-
-  if (req.nextUrl.pathname.startsWith('/admin')) {
-    if (!accessToken) return NextResponse.redirect(new URL('/auth/login', req.url));
-    const tokenPayload = Jwt.getPayload(accessToken.value);
-    if (tokenPayload && !tokenPayload.id) return NextResponse.rewrite(new URL('/404', req.url));
-
-    userApi
-      .getUserUserId(UserTokenUtil.getTokenBearer(accessToken.value), tokenPayload.id)
-      .then(({ data: user }) => {
-        if (user.role === UserRoleEnum.Admin) return NextResponse.next();
-        throw new Error('Not admin');
-      })
-      .catch(() => {
-        return NextResponse.rewrite(new URL('/404', req.url));
-      });
-  }
-
   // Get lng from cookie or Accept-Language header
   let lng;
   if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName)!.value);
@@ -57,6 +35,6 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api/|_next/static|_next/image|favicon.ico|sw.js|site.webmanifest|icons/|splashscreens/|auth/|admin/|blog|.*\\.[a-zA-Z0-9]).*)',
+    '/((?!api/|_next/static|_next/image|favicon.ico|sw.js|site.webmanifest|icons/|splashscreens/|blog|.*\\.[a-zA-Z0-9]).*)',
   ],
 };
