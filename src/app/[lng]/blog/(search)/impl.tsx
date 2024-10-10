ca6bc6994d86324dbc25ed3ac5502da75ee16f8c
DOHYUK YOU <okdohyuk@gmail.com>
2024-10-10 22:26:42 +0900

    fix: remove comments

--- src/app/[lng]/blog/(search)/impl.tsx
+++ src/app/[lng]/blog/(search)/impl.tsx
@@ -7,16 +7,6 @@ import { Language } from '~/app/i18n/settings';
 import { observer } from 'mobx-react';
 import { BlogCategory } from '@api/Blog';
 
-/**
- * TODO
- * 블로그 app라우팅
- *  * 작성페이지
- * pwa 고치기
- * 에널리틱스 고치기
- * 해몽 서버컴포넌트
- *
- */
-
 const BlogImpl = ({
   params: { lng },
   category,
