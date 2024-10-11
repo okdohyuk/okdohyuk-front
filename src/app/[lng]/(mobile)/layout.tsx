dba0a8d508a05538275e08c486c540ae5123aaca
DOHYUK YOU <okdohyuk@gmail.com>
2024-10-11 19:47:11 +0900

    fix: 광고 높이 설정

--- src/app/[lng]/(mobile)/layout.tsx
+++ src/app/[lng]/(mobile)/layout.tsx
@@ -7,13 +7,13 @@ export default function MobileLayout({ children }: { children: React.ReactNode }
     <AsideScreenWrapper
       left={
         <GoogleAd
-          className={'w-48 h-[500px] mt-8 mr-4 ml-auto sticky top-20 bg-basic-3'}
+          className={'w-48 min-h-80 h-fit mt-8 mr-4 ml-auto sticky top-20 bg-basic-3'}
           slotId={'9185479703'}
         />
       }
       right={
         <GoogleAd
-          className={'w-48 h-[500px] mt-8 ml-4 mr-auto sticky top-20 bg-basic-3'}
+          className={'w-48 min-h-80 h-fit mt-8 ml-4 mr-auto sticky top-20 bg-basic-3'}
           slotId={'8803068842'}
         />
       }
