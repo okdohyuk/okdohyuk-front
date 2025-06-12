be1380a370dee9a2bd5034366163f5700a7b27dd
DOHYUK YOU <okdohyuk@gmail.com>
2025-06-12 12:15:04 +0900

    fix: 서버 시계 페이지의 props 타입 개선

--- src/app/[lng]/(mobile)/server-clock/page.tsx
+++ src/app/[lng]/(mobile)/server-clock/page.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { Language } from '~/app/i18n/settings';
 import Clock from './components/Clock';
 import { GenerateMetadata, translationsMetadata } from '@libs/server/customMetadata';
-
-interface ServerClockPageProps {
-  params: { lng: Language };
-}
+import { LanguageParams } from '~/app/[lng]/layout';
 
 export const generateMetadata: GenerateMetadata = async ({ params }) =>
   translationsMetadata({ params, ns: 'server-clock' });
 
-export default async function ServerClockPage({ params }: ServerClockPageProps) {
-  const lng = await params.lng;
-  // const { t } = await getTranslations(lng, 'server-clock');
+export default async function ServerClockPage({ params }: LanguageParams) {
+  const { lng } = await params;
 
   return <Clock lng={lng} />;
 }
