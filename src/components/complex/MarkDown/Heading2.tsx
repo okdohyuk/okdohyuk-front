ef176a9a157657655bf580cf8295ba3d607a1814
Dohyuk You <okdohyuk@gmail.com>
2026-02-07 05:34:56 +0900

    fix: header

--- src/components/complex/MarkDown/Heading2.tsx
+++ src/components/complex/MarkDown/Heading2.tsx
@@ -9,10 +9,13 @@ const Heading2: MarkdownComponent = function Heading2({ children }) {
   const urlSlug = StringUtils.toUrlSlug(extractTextFromReactNode(children));
 
   return (
-    <h2 id={urlSlug} className={cn('group mt-10 text-2xl font-black text-black')}>
+    <h2
+      id={urlSlug}
+      className={cn('group mt-10 text-2xl font-black text-zinc-900 dark:text-zinc-100')}
+    >
       <Link
         href={`#${urlSlug}`}
-        className="relative inline-block no-underline !text-black hover:!text-black"
+        className="relative inline-block no-underline !text-zinc-900 hover:!text-zinc-900 dark:!text-zinc-100 dark:hover:!text-zinc-100"
       >
         <span className="pointer-events-none absolute -left-5 top-1/2 hidden -translate-y-1/2 text-point-1 opacity-0 transition-opacity md:block md:group-hover:opacity-100">
           #
