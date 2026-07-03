fbcf7e0bb5a967eed5d867ab20dc35900bd0ce52
Dohyuk You <okdohyuk@gmail.com>
2026-07-03 18:41:45 +0900

    fix: Textarea 가로스크롤 버그 제거
    - 공용 Textarea가 <textarea>에 flex+items-center를 적용해 네이티브
      soft-wrap이 깨지고 긴 문자열에서 가로 스크롤 발생
    - display를 block으로 되돌리고 whitespace-pre-wrap/break-words 명시
    - 방명록 포함 Textarea 사용 16개 도구 전반에 적용되는 루트 수정
    - Playwright 실측: hasHorizontalScroll=false, 긴 문자열 세로 줄바꿈 확인

--- src/components/basic/Textarea/Textarea.tsx
+++ src/components/basic/Textarea/Textarea.tsx
@@ -8,10 +8,11 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          'flex min-h-[32px] w-full items-center p-1 border border-basic-3 rounded-lg ' +
+          'block min-h-[32px] w-full p-1 border border-basic-3 rounded-lg ' +
             'bg-basic-0 text-fg-1 ' +
             'focus:ring-2 focus:ring-point-1 focus:border-transparent outline-none ' +
-            'transition-all duration-200 resize-none',
+            'transition-all duration-200 resize-none ' +
+            'whitespace-pre-wrap break-words',
           className,
         )}
         ref={ref}
