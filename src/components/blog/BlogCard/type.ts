2c4fa2b2207154de2e90b639486df8a140490826
Dohyuk You <okdohyuk@gmail.com>
2026-07-11 21:34:18 +0900

    fix: 어드민 블로그 게시 후 목록에서 카드 클릭이 동작하지 않는 문제 해결
    - submitBlog redirect와 어드민 BlogCard 링크에 로케일(lng) prefix 명시
    - 로케일 없는 경로가 미들웨어 307을 거치며 게시 직후 라우터 상태가
      어긋나 Link 클릭이 무시되던 문제
    - redirect()를 try/catch 밖으로 이동(NEXT_REDIRECT가 catch를 타지 않도록)

--- src/components/blog/BlogCard/type.ts
+++ src/components/blog/BlogCard/type.ts
@@ -7,6 +7,8 @@ type BlogCardProps = {
   blog: BlogSearch;
   isAdmin?: boolean;
   type?: BlogCardType;
+  /** 지정 시 로케일 prefix가 붙은 경로로 이동해 미들웨어 리다이렉트를 거치지 않는다 */
+  lng?: string;
 };
 
 type BlogCardTypeProps = {
