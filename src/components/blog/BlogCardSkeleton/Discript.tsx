d389de085823b57f1df2c8d1e04dd332ece89c36
Dohyuk You <okdohyuk@gmail.com>
2026-07-27 17:36:30 +0900

    fix: 블로그 카드 스켈레톤을 실제 카드 줄 구조에 맞춰 정렬
    제목은 모바일 1줄 / 데스크탑 2줄, 본문은 line-clamp와 동일한 줄 수로
    분리하고 마지막 줄 폭을 줄여 텍스트가 중간에 잘림을 드러내도록 함

--- src/components/blog/BlogCardSkeleton/Discript.tsx
+++ src/components/blog/BlogCardSkeleton/Discript.tsx
@@ -7,12 +7,23 @@ function Discript() {
     <article
       className={`${BLOG_GLASS_PANEL_SOFT} flex gap-4 overflow-hidden p-3 md:min-h-[200px] md:gap-6 md:p-4`}
     >
-      <Skeleton className="relative h-[108px] w-[108px] rounded-xl md:h-[196px] md:w-[196px]" />
+      <Skeleton className="relative h-[108px] w-[108px] shrink-0 rounded-xl md:h-[196px] md:w-[196px]" />
 
-      <div className="flex flex-col flex-1 text-left justify-start overflow-hidden">
-        <Skeleton className="my-1 md:my-4" h={6} />
-        <Skeleton h={16} />
-        <Skeleton className="mt-auto md:mb-4" h={4} w={12} />
+      <div className="flex flex-1 flex-col justify-start overflow-hidden text-left">
+        {/* 제목: 모바일 1줄 / 데스크탑 최대 2줄. 둘째 줄은 짧게 두어 잘린 제목임을 암시 */}
+        <div className="my-1 flex flex-col gap-2 md:my-3">
+          <Skeleton className="h-4 rounded" />
+          <Skeleton className="hidden h-4 w-1/2 rounded md:block" />
+        </div>
+
+        {/* 본문: 모바일 2줄 / 데스크탑 3줄 (line-clamp와 동일한 줄 수) */}
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-3.5 rounded" />
+          <Skeleton className="h-3.5 w-3/4 rounded md:w-full" />
+          <Skeleton className="hidden h-3.5 w-2/3 rounded md:block" />
+        </div>
+
+        <Skeleton className="mt-auto h-3 w-16 rounded md:mb-1" />
       </div>
     </article>
   );
--- src/components/blog/BlogCardSkeleton/Frame.tsx
+++ src/components/blog/BlogCardSkeleton/Frame.tsx
@@ -7,10 +7,22 @@ function Frame() {
     <article className={`${BLOG_GLASS_PANEL_SOFT} flex h-80 flex-col overflow-hidden md:h-96`}>
       <Skeleton className="relative h-40 w-full md:h-44" />
 
-      <div className="flex flex-col flex-1 p-4">
-        <Skeleton h={6} />
-        <Skeleton className="mt-2" h={16} />
-        <Skeleton className="mt-auto" h={4} w={12} />
+      <div className="flex flex-1 flex-col p-4">
+        {/* 제목: 모바일 1줄 / 데스크탑 최대 2줄. 둘째 줄은 짧게 두어 잘린 제목임을 암시 */}
+        <div className="flex flex-col gap-2">
+          <Skeleton className="h-4 rounded" />
+          <Skeleton className="hidden h-4 w-3/5 rounded md:block" />
+        </div>
+
+        {/* 본문: 모바일 4줄 / 데스크탑 3줄 (line-clamp와 동일한 줄 수) */}
+        <div className="mt-2 flex flex-col gap-2">
+          <Skeleton className="h-3.5 rounded" />
+          <Skeleton className="h-3.5 rounded" />
+          <Skeleton className="h-3.5 w-4/5 rounded md:w-2/3" />
+          <Skeleton className="h-3.5 w-2/5 rounded md:hidden" />
+        </div>
+
+        <Skeleton className="mt-auto h-3 w-16 rounded" />
       </div>
     </article>
   );
